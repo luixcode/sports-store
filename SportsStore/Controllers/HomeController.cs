3e3c24f882b61d120cec015c8b1f0f6b36bab3cd
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-09 22:02:32 -0400

    Update /Controllers/HomeController.cs

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
         public IActionResult Index() => View();
     }
 }
