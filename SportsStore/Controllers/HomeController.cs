460be2bfc12e72a361fa3d700281016cdcb6585d
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-10 20:46:30 -0400

    Adding Navigation Controls

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -19,11 +19,12 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult Index(int productPage = 1)
+        public ViewResult Index(string category, int productPage = 1)
         {
             return View(new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((productPage - 1) * PageSize)
                     .Take(PageSize),
@@ -32,7 +33,8 @@ namespace SportsStore.Controllers
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             });
         }
     }
