00920893efab73b93ea6e65d1c8af5140d275316
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-11 20:52:42 -0400

    Update Startup.cs

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -34,6 +34,7 @@ namespace SportsStore
             });
 
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
