74c2afc5206736e73e48fea75afc374b6552c005
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-13 17:07:07 -0400

    Add CRUD methods

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -12,5 +12,20 @@ namespace SportsStore.Models
         public EFStoreRepository(StoreDbContext ctx) => context = ctx;
 
         public IQueryable<Product> Products => context.Products;
+
+        public void CreateProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+        public void DeleteProduct(Product p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+        public void SaveProduct(Product p)
+        {
+            context.SaveChanges();
+        }
     }
 }
