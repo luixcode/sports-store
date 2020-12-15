221ad044a76867683f83090173cb430a8a905653
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-14 21:45:10 -0400

    Configure error handling

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -52,6 +52,16 @@ namespace SportsStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/error");
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
